eff484a396b3bbe699cee3ddbafc1f725f6e28a3
Arash Pakbin <a.pakbin@cse-stmi-s1.cse.tamu.edu>
2022-01-02 22:46:39 -0600

    DL edits

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from utils import timer, run_as_process, TrueHaz, calc_L2, create_dir_if_not_exi
 
 # BoXHED 2.0 (https://arxiv.org/pdf/2103.12591.pdf) is a software package
 # for estimating hazard functions nonparametrically via gradient boosting. 
-# It dramatically improves upon BoXHED 1.0 (http://proceedings.mlr.press/v119/wang20o/wang20o.pdf) in speed.
+# It is orders of magnitude faster than BoXHED 1.0 (http://proceedings.mlr.press/v119/wang20o/wang20o.pdf).
 # BoXHED 2.0 also allows for more general forms of survival data including recurrent events.
 
 #This tutorial demonstrates how to apply BoXHED 2.0 to a synthetic dataset.
@@ -14,15 +14,15 @@ from utils import timer, run_as_process, TrueHaz, calc_L2, create_dir_if_not_exi
 DATA_ADDRESS = "./data/"     # train/test data directory
 RSLT_ADDRESS = "./results/"  # results directory
 
-nthread_prep    = 20    # number of threads used for preprocessing
-nthread_train   = 20    # number of threads used for training
+nthread_prep    = 20    # number of CPU threads used for preprocessing
+nthread_train   = 20    # number of CPU threads used for training
 
-# creating the results' directory if it does not exist
+# Create the results' directory if it does not exist
 for addr in [RSLT_ADDRESS]:
     create_dir_if_not_exist(addr)
 
 # Function: read_train_data
-# reads the synthetic training data.
+# Reads the synthetic training data.
 # Input:
 #      None
 # Return: 
@@ -32,7 +32,7 @@ for addr in [RSLT_ADDRESS]:
 #            * t_end:   the end time of the epoch
 #            * X_i:     values of covariates between t_start and t_end
 # Sample Output:
-# subject	t_start         t_end           X_0             delta
+# ID	t_start         t_end           X_0             delta
 #	1       0.010000	0.064333	0.152407	0.0
 #	1       0.064333	0.135136	0.308475	0.0
 #	1       0.194810	0.223106	0.614977	1.0
@@ -41,19 +41,22 @@ for addr in [RSLT_ADDRESS]:
 #	2       0.841729	0.886587	0.196407	0.0
 #	2       0.886587	0.949803	0.671227	0.0
 #
-# As can be seen above, t_start<T_end for each epoch (row). Also, the beginning of one epoch starts no earlier than
-# the end of the previous one, i.e. t_end_i <= t_start_i+1 . Delta denotes whether an event (possibly recurrent) 
-# occurred at the end of the epoch. For covariates with missing values, BoXHED 2.0 implements tree splits of the form:
+# For each epoch (row) we must have t_start < t_end. Also, the beginning of one epoch cannot start earlier than
+# the end of the previous one, i.e. t_end_i <= t_start_{i+1}.
+#
+# Delta denotes whether an event (possibly recurring) occurred at the end of the epoch.
+#
+# For covariates with missing values, BoXHED 2.0 implements tree splits of the form:
 # Left daughter node: {x<=split.point or x is missing}; Right daughter node: {x>split.point}
 # or
 # Left daughter node: {x<=split.point}; Right daughter node: {x>split.point or x is missing}.
-# Alternatively, the user may choose to impute the missing values, for example by carrying forward the most recent observed value.
+# Alternatively, missing values may be manually imputed, e.g. by carrying forward the most recent value.
 def read_train_data():
     return pd.read_csv(os.path.join(DATA_ADDRESS, 'training.csv'))
 
 
 # Function: read_test_data
-# reads the synthetic testing data. The values of the true hazard function are also provided for accuracy comparisons.
+# Reads the synthetic testing data. The values of the true hazard function are also provided for accuracy comparisons.
 # Input:
 #      None
 # Return: 
@@ -67,63 +70,61 @@ def read_test_data():
 
 
 # Function: cv_train_test_BoXHED2
-# cross-validate, train, and test a BoXHED2.0 instance
+# Select hyperparameters by K-fold cross-validation, and then fit the BoXHED2.0 hazard estimator
 # Input:
 #      None
 # Return: 
 #      @ A dictionary containing RMSE (with 95% CIs) and timings of different components.
 @run_as_process
 def cv_train_test_BoXHED2():
-    # defining the output dictionary
+    # Define the output dictionary
     out_dict = {}
 
-    #reading in the data
+    #Read in the training data
     data = read_train_data()
 
-    # Creating an instance of BoXHED to preprocess the training data.
-    boxhed_ = boxhed()
-
-    # preprocessing. We are also timing it.
-    prep_timer = timer()                          # initializing a timer.
+    # Preprocess the training data. THIS ONLY NEEDS TO BE DONE ONCE.
+    boxhed_ = boxhed()                            # Create an instance of BoXHED
+    prep_timer = timer()                          # Initialize timer
     # boxhed.preprocess():
     # Input:
     #      @ num_quantiles: the number of candidate split points to try for time and for each covariate. 
     #                       The locations of the split points are based on the quantiles of the training data.
     #      @ is_cat:        a list of the column indexes that contain categorical data. The categorical data must be one-hot encoded.
-    #                       For example, if a categorical variable with 3 factors is one hot encoded into binary-valued columns 4,5,6, 
-    #                       then is_cat = [4,5,6]
+    #                       For example, is_cat = [4,5,6] if a categorical variable with 3 factors is transformed into binary-valued columns 4,5,6
     #      @ weighted:      if set to True, the locations of the candidate split points will be based on weighted quantiles 
     #                       (see Section 3.3 of the BoXHED 2.0 paper)      
     #      @ nthreads:      number of CPU threads to use for preprocessing the data
     # Return: 
-    #      @ ID:            subject ID for each row in the data frames X, w, and delta
+    #      @ ID:            subject ID for each row in the processed data frames X, w, and delta
     #      @ X:             each row represents an epoch of the transformed data, and contains the values of the covariates as well as
     #                       its start time
     #      @ w:             length of each epoch     
     #      @ delta:         equals one if an event occurred at the end of the epoch; zero otherwise
-    num_quantiles   = 256
     ID, X, w, delta = boxhed_.preprocess(
             data          = data, 
             #is_cat       = [],
-            num_quantiles = num_quantiles, 
+            num_quantiles = 256, 
             weighted      = False, 
             nthread       = nthread_prep)
     out_dict["prep_time"] = prep_timer.get_dur()  # calling the get_dur() function.
     
-    # perform K-fold cross-validation to choose the hyperparameters {number of boosted trees, tree depth, learning rate} if do_CV is true.
-    # first, specify the candidate values for the hyperparameters to cross-validate on (more trees and/or deeper trees may be needed for other datasets).
+    # Perform K-fold cross-validation to select hyperparameters {number of trees, tree depth, learning rate} if do_CV = True.
+    # Otherwise, users should manually specify hyperparameter values. Note that a tree of depth k has 2^k leaf nodes.
+    do_CV = False                                 
+    param_manual = {'max_depth':1, 'n_estimators':200}
+    
+    # Specify the candidate values for the hyperparameters to cross-validate on (more trees and/or deeper trees may be needed for other datasets).
     param_grid = {'max_depth':    [1, 2, 3, 4, 5],
               'n_estimators': [50, 100, 150, 200, 250, 300]}
-    # if do_CV is false, use the following default values:
-    hyperparams = {'max_depth':1, 'n_estimators':200}
-    do_CV           = False # if True, cross validation is performed for best hyperparameters. Default values are used otherwise.
-    # next, specify:
-    #      @ gpu_list:    the list of GPU IDs to use for training (we set gpu_list to [-1] to use CPU in this tutorial).
-    #      @ batch_size : training batch size, which is the maximum number of BoXHED2.0 instances trained at any point in time. 
-    #                     If we perform 10-fold cross-validation using the above param_grid, we would need to train 5 * 6 * 10 = 300
+    
+    # Next, specify:
+    #      @ gpu_list:    the list of GPU IDs to use for training. Set gpu_list = [-1] to use CPUs.
+    #      @ batch_size:  training batch size, which is the maximum number of BoXHED2.0 instances trained at any point in time. 
+    #                     If we perform 10-fold cross-validation using the param_grid above, we would need to train 5 * 6 * 10 = 300
     #                     instances in total
     #                           * When using GPUs, each GPU will train at most batch_size/len(gpu_list) instances at a time
-    #                           * When gpu_list = [-1], batch_size is the number of CPU threads used, 
+    #                           * When gpu_list = [-1], batch_size should be the number of CPU threads used, 
     #                             with each one training one instance at a time
     gpu_list   = [-1]
     batch_size = 6
@@ -135,8 +136,9 @@ def cv_train_test_BoXHED2():
         # Return: 
         #      @ cv_rslts:    mean and st.dev of the log-likelihood value for each hyperparameter combination
         #      @ best_params: The hyper-parameter combination where the mean log-likelihood value is maximized.
-        #                     HOWEVER, we recommend using the one-standard-error rule to select the most parsimonious 
-        #                     combination that is within st.dev/sqrt(k) of the maximum log-likelihood value (§7.10 of Hastie et al. (2009))
+        #                     HOWEVER, we strongly recommend using the one-standard-error rule to select the most parsimonious 
+        #                     combination that is within st.dev/sqrt(k) of the maximum log-likelihood value.
+        #                     See §7.10 in 'Elements of Statistical Learning' by Hastie et al. (2009).
         cv_rslts, best_params = cv(param_grid, 
                                   X, 
                                   w,
@@ -148,23 +150,25 @@ def cv_train_test_BoXHED2():
     
         out_dict["CV_time"] = cv_timer.get_dur()
     else:
-        best_params = hyperparams
+        best_params = param_manual
     best_params['gpu_id'] = gpu_list[0] # holding on to one GPU id to use for training
     best_params['nthread'] = nthread_train 
 
     out_dict.update(best_params)
-    boxhed_.set_params (**best_params)
+    boxhed_.set_params(**best_params)
 
+    
+    # Fit BoXHED to the training data
     fit_timer = timer()
-    # Fit BoXHED to the training data:
-    boxhed_.fit (X, delta, w)
+    boxhed_.fit(X, delta, w)
     out_dict["fit_time"] = fit_timer.get_dur()
 
     # Load the test set and the values of the true hazard function at the test points:
     test_X, test_true_haz = read_test_data()
 
-    pred_timer = timer()
+   
     # Use the fitted model to estimate the value of the hazard function for each row of the test set:
+    pred_timer = timer()
     preds = boxhed_.predict(test_X)
     out_dict["pred_time"] = pred_timer.get_dur()
 
